49c8bfb4bae3690cad3498aec7e18703f44d9fc5
yukti <211b361@juetguna.in>
2023-06-02 00:03:40 +0530

    This is the first commit for the reminder app which includes layout of login,registration and task page

---
+++ app/src/main/java/com/example/reminder_app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.reminder_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.animation.core.Animation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textview=findViewById(R.id.textView);
+
+        ImageView imageview=findViewById(R.id.alarm_image);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/reminder_app/registration_page.java
@@ -0,0 +1,14 @@
+package com.example.reminder_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page);
+    }
+}
